5c7264ed8f4db6f9847886e29cfcfe5591018273
lexeg <klim2041@yandex.ru>
2016-10-01 10:01:47 +0500

    Сделаны изменения в файле web.php. Добавлен роут, чтобы работал gii. Добавлена папка modules для отделения админки. Добавлен CRUD для Groups и Roles

--- config/web.php
+++ config/web.php
@@ -6,6 +6,12 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'ru-RU',
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -43,6 +49,7 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<module>/<controller>/<action>' => '<module>/<controller>/<action>',
                 '<controller>/<action>' => '<controller>/<action>',
                 '<action>' => 'site/<action>'
             ],
